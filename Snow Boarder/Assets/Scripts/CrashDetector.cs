347d4566a7ac9d3e78c7b9da75bfb1df885e13ad
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2021-10-21 20:48:02 +0200

    Triggers to restart levels

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if(other.tag == "Ground")
+        Debug.Log("My head !! ");
+
+
+    }
+
+}
